c5aa8fe41b8e21f7b0a792c34fa057af29ad23a1
vijism89 <47097126+vijism89@users.noreply.github.com>
2019-05-14 16:51:53 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,1 +1,33 @@
+--Get all customers and their addresses.
+SELECT * FROM "customers"
+JOIN "addresses" ON customers.id = addresses.customer_id;
+
+--Get all orders and their line items (orders, quantity and product).
+SELECT * FROM "orders" 
+JOIN "line_items" ON orders.id = line_items.order_id;
+
+--Which warehouses have cheetos?
+ SELECT "warehouse" FROM warehouse
+ JOIN "warehouse_product" ON warehouse_product.warehouse_id = warehouse.id
+ JOIN "products" ON products.id = warehouse_product.product_id 
+ WHERE "products".description = 'cheetos';
+ 
+ --Which warehouses have diet pepsi?
+ SELECT "warehouse" FROM warehouse
+ JOIN "warehouse_product" ON warehouse_product.warehouse_id = warehouse.id
+ JOIN "products" ON products.id = warehouse_product.product_id 
+ WHERE "products".description = 'diet pepsi';
+ 
+ Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+ SELECT "customers".id,"customers".first_name, "customers".last_name, 
+ COUNT("orders".id) FROM "customers"
+ JOIN "addresses" ON customers.id = addresses.customer_id
+ FULL JOIN "orders" ON orders.address_id = addresses.id
+ GROUP BY "customers".id;
+ 
+ How many customers do we have?
+SELECT COUNT("customers".id) FROM "customers";
+ 
+ How many products do we carry?
+SELECT COUNT("products".id) FROM "products";
 
